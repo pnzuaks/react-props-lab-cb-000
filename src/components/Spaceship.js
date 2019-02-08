f79c694686492520f8d9f4b57294d3fbf3ed7fb2
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-08 22:00:11 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -8,8 +8,12 @@ export default class Spaceship extends React.component {
         <h4>Speed: { this.props.speed }</h4>
         <h4>Rockets: { this.props.hasRockets }</h4>
         <h4>Colors: { this.props.colors.join(', ')}</h4>
-
       </div>
     )
   }
+}
+Spaceship.defaultProps = {
+  speed: 'slow',
+  hasRockets: false,
+  colors: ['black', 'red']
 }
