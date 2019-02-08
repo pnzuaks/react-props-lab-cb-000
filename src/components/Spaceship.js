774ed643ccb2bdcfa89d23690d318f6abec137a4
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-08 21:56:19 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,7 +1,15 @@
+import React, { Component } from 'react';
+
 export default class Spaceship extends React.component {
   render() {
     return (
-      <form> 
+      <div>
+        <h1>{ this.props.name }</h1>
+        <h4>Speed: { this.props.speed }</h4>
+        <h4>Rockets: { this.props.hasRockets }</h4>
+        <h4></h4>
+        
+      </div>
     )
   }
 }
