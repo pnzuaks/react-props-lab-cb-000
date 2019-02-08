92570c20d83543025875e289a45a69d378011687
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-08 21:57:22 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -7,8 +7,8 @@ export default class Spaceship extends React.component {
         <h1>{ this.props.name }</h1>
         <h4>Speed: { this.props.speed }</h4>
         <h4>Rockets: { this.props.hasRockets }</h4>
-        <h4></h4>
-        
+        <h4>Colors: { this.props.colors.join(', ')}</h4>
+
       </div>
     )
   }
